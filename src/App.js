fcf02579d8cf9987d549b6d1ce33bff03b26c9ef
alireza ettehadi <alireza.ettehadi9595@gmail.com>
2022-01-10 13:52:20 +0330

    add routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,44 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
+import NotFoundPage from "./containers/404";
+import Home from "./containers/Home";
+import SignIn from "./containers/SignIn";
+import SignUp from "./containers/SignUp";
+import Dashboard from "./containers/Dashboard";
 
 function App() {
   return (
-    <div>
-      hello world!
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/signup">signup</Link>
+            </li>
+            <li>
+              <Link to="/signin">signin</Link>
+            </li>
+            <li>
+              <Link to="/dashboard">dashboard</Link>
+            </li>
+            <li>
+              <Link to="/asd">Users</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route path="*" element={<NotFoundPage />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
