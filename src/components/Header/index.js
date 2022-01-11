ae5e93824af98579e96529249dee99a2f266668a
alireza ettehadi <alireza.ettehadi9595@gmail.com>
2022-01-11 16:57:37 +0330

    update header and layout

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,26 +1,38 @@
 import React from "react";
 import { Button } from "react-bootstrap";
-import {  Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
-export default function Header() {
+export default function Header({ user }) {
   return (
     <div
       className="header w-100 p-4  d-flex align-items-center justify-content-between"
       style={{ backgroundColor: "#5CB3FF" }}
     >
-      <div>
-        <Link to="/">
-          <Button variant="secondary">Log Out</Button>
-        </Link>
-      </div>
-      <div>
-        <Link to="/signup" className="me-3">
-          <Button variant="secondary">Sign Up</Button>
-        </Link>
-        <Link to="/signin">
-          <Button variant="secondary">Sign In</Button>
-        </Link>
-      </div>
+      {user?.accessToken ? (
+        <div>
+          <Link to="/">
+            <Button variant="secondary">Log Out</Button>
+          </Link>
+        </div>
+      ) : (
+        <div></div>
+      )}
+      {!user?.accessToken ? (
+        <div>
+          <Link to="/signup" className="me-3">
+            <Button variant="secondary">Sign Up</Button>
+          </Link>
+          <Link to="/signin">
+            <Button variant="secondary">Sign In</Button>
+          </Link>
+        </div>
+      ) : (
+        <div>
+          <Link to="/dashboard">
+            <Button variant="secondary">dashboard</Button>
+          </Link>
+        </div>
+      )}
     </div>
   );
 }
--- src/layout/index.js
+++ src/layout/index.js
@@ -2,14 +2,28 @@ import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
-export default function Layout({ children }) {
+import { connect } from "react-redux";
+function Layout({ children , user }) {
   return (
     <div>
       <Router>
-        <Header />
+        <Header user={user} />
         {children}
         <Footer />
       </Router>
     </div>
   );
 }
+
+function mapStateToProps(state) {
+  return {
+    user: state.users.user,
+  };
+}
+function mapDispatchToProps(dispatch) {
+  return {
+    dispatch
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Layout);
